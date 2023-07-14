e7c7224d0c8c1cd2cba3f5c447db830a6762d898
bwpge <bwpge.dev@gmail.com>
2023-07-14 18:00:55 -0400

    Add transfer and flag tests

--- tests/instructions.rs
+++ tests/instructions.rs
@@ -5,6 +5,33 @@ use lib6502::Bus;
 use crate::common::setup;
 
 #[cfg(test)]
+mod flags {
+    use super::*;
+
+    #[test]
+    fn clear() {
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0x18 0xD8 0x58 0xB8; // CLC CLD CLI CLV  8 cyc
+        });
+        assert_cpu!(cpu, pc = 0x0000, p = 0x24, cyc = 7);
+
+        cpu.step_for(4);
+        assert_cpu!(cpu, pc = 0x0004, p = 0x20, cyc = 15);
+    }
+
+    #[test]
+    fn set() {
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0x38 0xF8 0x78; // SEC SED SEI  6 cyc
+        });
+        assert_cpu!(cpu, pc = 0x0000, p = 0x24, cyc = 7);
+
+        cpu.step_for(3);
+        assert_cpu!(cpu, pc = 0x0003, p = 0x2D, cyc = 13);
+    }
+}
+
+#[cfg(test)]
 mod jmp {
     use super::*;
 
@@ -288,6 +315,223 @@ mod lda {
             y = 0x01,
             p = 0x24,
             cyc = 15
+        );
+    }
+}
+
+#[cfg(test)]
+mod transfer {
+    use super::*;
+
+    #[test]
+    fn tax() {
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0xAA; //       TAX       2 cyc
+            0x0001: 0xA9 0x0F; //  LDA #$0F  2 cyc
+            0x0003: 0xAA; //       TAX       2 cyc
+        });
+        assert_cpu!(
+            cpu,
+            pc = 0x0000,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x24,
+            cyc = 7
+        );
+
+        cpu.step();
+        assert_cpu!(
+            cpu,
+            pc = 0x0001,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x26,
+            cyc = 9
+        );
+
+        cpu.step_for(2);
+        assert_cpu!(
+            cpu,
+            pc = 0x0004,
+            a = 0x0F,
+            x = 0x0F,
+            y = 0x00,
+            p = 0x24,
+            cyc = 13
+        );
+    }
+
+    #[test]
+    fn tay() {
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0xA8; //       TAY       2 cyc
+            0x0001: 0xA9 0x0F; //  LDA #$0F  2 cyc
+            0x0003: 0xA8; //       TAY       2 cyc
+        });
+        assert_cpu!(
+            cpu,
+            pc = 0x0000,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x24,
+            cyc = 7
+        );
+
+        cpu.step();
+        assert_cpu!(
+            cpu,
+            pc = 0x0001,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x26,
+            cyc = 9
+        );
+
+        cpu.step_for(2);
+        assert_cpu!(
+            cpu,
+            pc = 0x0004,
+            a = 0x0F,
+            x = 0x00,
+            y = 0x0F,
+            p = 0x24,
+            cyc = 13
+        );
+    }
+
+    #[test]
+    fn tsx() {
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0xBA; //  TSX  2 cyc
+        });
+        assert_cpu!(
+            cpu,
+            pc = 0x0000,
+            s = 0x01FD,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x24,
+            cyc = 7
+        );
+
+        cpu.step();
+        assert_cpu!(
+            cpu,
+            pc = 0x0001,
+            s = 0x01FD,
+            a = 0x00,
+            x = 0xFD,
+            y = 0x00,
+            p = 0xA4,
+            cyc = 9
+        );
+    }
+
+    #[test]
+    fn txa() {
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0x8A; //       TXA       2 cyc
+            0x0001: 0xA2 0x0F; //  LDX #$0F  2 cyc
+            0x0003: 0x8A; //       TXA       2 cyc
+        });
+        assert_cpu!(
+            cpu,
+            pc = 0x0000,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x24,
+            cyc = 7
+        );
+
+        cpu.step();
+        assert_cpu!(
+            cpu,
+            pc = 0x0001,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x26,
+            cyc = 9
+        );
+
+        cpu.step();
+        assert_cpu!(
+            cpu,
+            pc = 0x0003,
+            a = 0x00,
+            x = 0x0F,
+            y = 0x00,
+            p = 0x24,
+            cyc = 11
+        );
+
+        cpu.step();
+        assert_cpu!(
+            cpu,
+            pc = 0x0004,
+            a = 0x0F,
+            x = 0x0F,
+            y = 0x00,
+            p = 0x24,
+            cyc = 13
+        );
+    }
+
+    #[test]
+    fn txs() {
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0x9A; //  TAY  2 cyc
+        });
+        assert_cpu!(cpu, pc = 0x0000, s = 0x01FD, x = 0x00, cyc = 7);
+
+        cpu.step();
+        assert_cpu!(cpu, pc = 0x0001, s = 0x0100, x = 0x00, cyc = 9);
+    }
+
+    #[test]
+    fn tya() {
+        // TODO
+        let (_, mut cpu) = setup(asm! {
+            0x0000: 0x98; //       TYA       2 cyc
+            0x0001: 0xA0 0x0F; //  LDY #$0F  2 cyc
+            0x0003: 0x98; //       TYA       2 cyc
+        });
+        assert_cpu!(
+            cpu,
+            pc = 0x0000,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x24,
+            cyc = 7
+        );
+
+        cpu.step();
+        assert_cpu!(
+            cpu,
+            pc = 0x0001,
+            a = 0x00,
+            x = 0x00,
+            y = 0x00,
+            p = 0x26,
+            cyc = 9
+        );
+
+        cpu.step_for(2);
+        assert_cpu!(
+            cpu,
+            pc = 0x0004,
+            a = 0x0F,
+            x = 0x00,
+            y = 0x0F,
+            p = 0x24,
+            cyc = 13
         );
     }
 }
